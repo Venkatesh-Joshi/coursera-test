b71ced5b9578f2bf123a8a09766ad17edd774826
Venkatesh-Joshi <56385929+Venkatesh-Joshi@users.noreply.github.com>
2020-06-24 01:44:58 +0530

    Update app.js

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -1,9 +1,57 @@
 (function () {
     'use strict';
     
-    angular.module('', [])
-    .controller('', );
+    angular.module('LunchCheck', [])
+    .controller('LunchCheckController', LunchCheckController);
+  
+    LunchCheckController.$inject = ['$scope'];
     
-   
+    function LunchCheckController($scope)
+     {
+
+        $scope.items = '';
+        $scope.message = '';
+        $scope.fontcolor ={};
+        $scope.bordercolor = {};
+
+        $scope.check = function (){
+            let arrayOfItems = $scope.items.split(',');
+            let counter = 0;
+
+            arrayOfItems.forEach((item) => {
+                if (item != false) {
+                    counter++;
+                }
+            });
+
+            if (counter > 0 && counter < 4){
+                $scope.message = 'Enjoy!';
+                $scope.fontcolor = {
+                    "color": "green"
+                  };
+                  $scope.bordercolor = {
+                    "border-color": "green"
+                  };
+            }
+            else if(counter >= 4){
+                $scope.message = 'Too much!';
+                $scope.fontcolor = {
+                    "color": "green"
+                  };
+                  $scope.bordercolor = {
+                    "border-color": "green"
+                  };                
+            }
+            else{
+                $scope.message = 'Please enter data first';
+                $scope.fontcolor = {
+                    "color": "red"
+                  };
+                  $scope.bordercolor = {
+                    "border-color": "red"
+                  };
+            }
+        }
+      }
     
     })();
